0d80e5810e4fffd5af879f5970e1f221e65244b4
Lennon Beato <lennonbe@vdi-sccubu-063.lancs.local>
2020-11-21 14:33:43 +0000

    About to change something, just backing up my code

--- coursework_211.c
+++ coursework_211.c
@@ -12,28 +12,40 @@ typedef struct Block
 }Block;
 
 static void* memory = 0;
+static void* left = 0;
+static void* right = 0;
+
+/*static Block* left;
+static Block* right;*/
 
 void split(Block* fitting_slot, size_t size) // these parameters are the block which we will be splitting and the size we will be using from this block
 {
-    Block* left = (void*)((void*)fitting_slot + sizeof(Block));
-    Block* right = (void*)((void*)fitting_slot + size + 2 * sizeof(Block));
+    left = (void*)((void*)fitting_slot + sizeof(Block));
+    right = (void*)((void*)fitting_slot + size + 2 * sizeof(Block));
 
-    fitting_slot->nextBlock = left;
+    Block* leftBlock = left;
+    Block* rightBlock = right;
 
-    left->size = size;
-    left->free = 0;
-    left->nextBlock = right;
-    left->prevBlock = fitting_slot;
+    leftBlock->size = size;
+    leftBlock->free = 0;
+    leftBlock->nextBlock = right;
+    leftBlock->prevBlock = fitting_slot -> prevBlock;
 
     /*
     These lines above show how the new block of memory in the structure is allocated. A new block of memory is created and its parameters are filled.
     This new Block struct will serve as the new free part of the structure, since its free variable is set to 1.
     */
 
-    right->size = (fitting_slot->size) - size - sizeof(Block);
-    right->free = 1;
-    right->nextBlock = fitting_slot->nextBlock;
-    right->prevBlock = left;
+    rightBlock->size = (fitting_slot->size) - size - sizeof(Block);
+    rightBlock->free = 1;
+    rightBlock->nextBlock = fitting_slot->nextBlock;
+    rightBlock->prevBlock = left;
+
+    printf("%p is free %d\n", leftBlock, leftBlock->free);
+    printf("%p is free %d\n", rightBlock, rightBlock->free);
+
+    //fitting_slot->nextBlock = left;
+    fitting_slot = left;
 
     /*
     Fitting slot will be used to store the used storage area. It will also connect with the new block through a nextBlock pointer in the structure.
@@ -44,24 +56,19 @@ void* new_malloc(size_t size)
 {
     Block* freeList = memory;
     void* result;
-    int memoryAmmount;
 
     if(memory == 0)
     {
-        Block* next;
+        static Block* next;
         memory = sbrk(8192);
 
         freeList = memory;
         next = memory + sizeof(Block)*2 + size;
 
-        //printf("%p %p is freeList & next \n", freeList, next);
-
         freeList->size = size; 
         freeList->free = 0;
         freeList->nextBlock = next;
         freeList->prevBlock = NULL; 
-
-        //printf("%p %p is freeList & next \n", freeList, freeList->nextBlock);
 
         next->free = 1;
         next->nextBlock = NULL;
@@ -74,10 +81,10 @@ void* new_malloc(size_t size)
     }
     else
     {
+        Block* temp = freeList;
         while(freeList/*->nextBlock*/ != NULL)
         {
             printf("%p \n", freeList);
-            //printf("%ld is freeList size \n", freeList->size);
 
             if(freeList->free == 1)
             {
@@ -94,9 +101,8 @@ void* new_malloc(size_t size)
                 else if((freeList->size) > size + sizeof(Block))
                 {
                     split(freeList, size);
-                    result = (void*)(++freeList);
+                    //result = (void*)(++freeList);
 
-                    printf("%p\n", result);
                     printf("Fitting block allocated with a split\n");
 
                     return result;
@@ -149,6 +155,7 @@ void* new_malloc(size_t size)
 int main()
 {
     new_malloc(920);
-    new_malloc(10);
+    new_malloc(120);
+    //new_malloc(10);
 
 }
